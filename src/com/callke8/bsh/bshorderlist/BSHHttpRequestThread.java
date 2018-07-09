9a87751cc0f4b7ebe2142e16abe3824131556585
hwzyyx <120077407@qq.com>
2018-07-09 11:21:05 +0800

    在  BSHHttpRequestThread.java，在反馈外呼结果时，每个平台反馈结果的地址是不一样的 通过数据字典进行配置，在这里调整原来由BSH的外呼参数配置，转由数据字典获取数据。

--- src/com/callke8/bsh/bshorderlist/BSHHttpRequestThread.java
+++ src/com/callke8/bsh/bshorderlist/BSHHttpRequestThread.java
@@ -1,8 +1,8 @@
 package com.callke8.bsh.bshorderlist;
 
-import com.callke8.bsh.bshcallparam.BSHCallParamConfig;
 import com.callke8.utils.BlankUtils;
 import com.callke8.utils.HttpClientUtil;
+import com.callke8.utils.MemoryVariableUtil;
 import com.callke8.utils.StringUtil;
 
 import net.sf.json.JSONObject;
@@ -17,6 +17,10 @@ public class BSHHttpRequestThread implements Runnable {
 	
 	private BSHCallResultVO bshCallResultVO;
 	private String id;
+	private BSHOrderList bshOrderList;
+	
+	private String bshCallBackUrl;          //呼叫结果反馈地址
+	private String bshCallBackKey;          //呼叫结果反馈密钥
 	
 	/**
 	 * 反馈呼叫结果
@@ -41,7 +45,23 @@ public class BSHHttpRequestThread implements Runnable {
 	public BSHHttpRequestThread(String id,String orderId,String callType,String callResult) {
 
 		this.id = id;
-		this.bshCallResultVO = new BSHCallResultVO(orderId, callType, callResult);
+		
+		this.bshOrderList = BSHOrderList.dao.getBSHOrderListById(id);     //根据ID，取出订单信息
+		
+		if(BlankUtils.isBlank(bshOrderList)) {                            //如果从数据库中取出的订单信息，如果订单信息为空时，直接返回
+			return;
+		}
+		
+		String channelSource = String.valueOf(bshOrderList.getInt("CHANNEL_SOURCE"));     //CHANNEL_SOURCE购物平台(1：京东平台;2：苏宁平台;3：天猫平台;4：国美平台)
+		
+		this.bshCallBackUrl = MemoryVariableUtil.getDictName("BSH_CALLBACK_URL", channelSource);      //呼叫结果反馈地址     
+		this.bshCallBackKey = MemoryVariableUtil.getDictName("BSH_CALLBACK_KEY", channelSource);      //呼叫结果反馈密钥
+		
+		if(BlankUtils.isBlank(bshCallBackUrl) || BlankUtils.isBlank(bshCallBackKey)) {                //只要反馈的地址和密钥为空，直接退出
+			return;
+		}
+		
+		this.bshCallResultVO = new BSHCallResultVO(orderId, callType, callResult,bshCallBackKey);
 
 	}
 	
@@ -52,14 +72,14 @@ public class BSHHttpRequestThread implements Runnable {
 		//发送呼叫结果至BSH 平台数据接口
 		JSONObject obj = JSONObject.fromObject(this.bshCallResultVO);
 		
-		String result = HttpClientUtil.doPost(BSHCallParamConfig.getBshCallResultUrl(),obj.toString());
+		String result = HttpClientUtil.doPost(this.bshCallBackUrl,obj.toString());
 		
 		//在提交外呼结果时，可能会出现通讯异常的情况，出现通讯异常时，DOB平台会返回 null, 
 		//不管是字符串的 null 还是 NULL,判定标准是，只要不包括 resultCode 或是 resultMsg 两个关键字，都表示通讯异常，需要再提交一次
 		
 		//如果第一次提交的结果为空,或是不包括 resultCode 时，再提交一次
 		if(BlankUtils.isBlank(result) || !StringUtil.containsAny(result, "resultCode")) {     
-			result = HttpClientUtil.doPost(BSHCallParamConfig.getBshCallResultUrl(),obj.toString());
+			result = HttpClientUtil.doPost(this.bshCallBackUrl,obj.toString());
 		}
 		
 		String callResultJson = this.bshCallResultVO.toString();
@@ -67,7 +87,7 @@ public class BSHHttpRequestThread implements Runnable {
 		
 		boolean b = BSHOrderList.dao.updateCallResultAndFeedBackCallResultRespond(id, callResultJson, feedBackCallResultRespond);
 		
-		System.out.println("将呼叫结果json数据:" + this.bshCallResultVO.toString() + " 反馈给BSH数据平台,BSH数据平台返回的结果是:" + result);
+		System.out.println("将呼叫结果json数据:" + this.bshCallResultVO.toString() + " 反馈给BSH数据平台,反馈结果URL：" + this.bshCallBackUrl + ",反馈结果密钥：" + this.bshCallBackKey + ",BSH数据平台返回的结果是:" + result);
 		
 	}
 
