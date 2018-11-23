23b20731c4d010652ed9ce82c3c2883c31e8c5e4
hwzyyx <120077407@qq.com>
2018-11-24 05:07:52 +0800

    在扫描待重呼数据到排队机时，由原来的3秒扫描调整为2秒。
    因为待重呼的优先级应该高于新外呼的数据。

--- src/com/callke8/predialqueueforautocallbyquartz/AutoCallLoadRetryJob.java
+++ src/com/callke8/predialqueueforautocallbyquartz/AutoCallLoadRetryJob.java
@@ -28,7 +28,7 @@ public class AutoCallLoadRetryJob implements Job {
 		StringBuilder sb = new StringBuilder();    //拼接日志
 		sb.append("====系统准备运行加载待重呼线程任务(AutoCallLoadRetryJob)====。现时排队机中排队数量为:" + queueCount + ",系统允许排队机最大数量为:" + queueMaxCount + "。");
 		
-		if(queueCount < queueMaxCount) {      //如果排队机中的数量已经大于或是等于允许的最大值，暂不加载数据到排队机
+		if(queueCount < queueMaxCount) {      //如果排队机中的数量已经大于或是等于允许的最大值，暂不加载数据到排队机 
 			
 			int freeQueueCount = queueMaxCount - queueCount;   //查看空闲的排队机空间
 			if(freeQueueCount > scanCount) {
@@ -46,10 +46,19 @@ public class AutoCallLoadRetryJob implements Job {
 			//遍历已激活的任务，取出任务 ID 列表，并以 taskId1,taskId2,taskId3 的形式拼接成字符串
 			StringBuilder idSB = new StringBuilder();
 			for(AutoCallTask act:activeCallTaskList) {
+				//并非只要处理激活状态的任务，就加入进行查询，而是必须要保证当前任务是否有待重呼的记录，即是要保证该任务的待重呼的记录数量大于0
 				String taskId = act.getStr("TASK_ID");   //取出 任务ID
-				idSB.append("\'" + taskId + "\',");
+				int retryCountTaskId = AutoCallTaskTelephone.dao.getAutoCallTaskTelephoneCountByState("3", taskId);    //查询该任务待重呼的数量
+				if(retryCountTaskId>0) {     //只有该任务待重呼数量大于0时，才加入
+					idSB.append("\'" + taskId + "\',");
+				}
 			}
 			String taskIds = idSB.toString();
+			if(BlankUtils.isBlank(taskIds)) {
+				sb.append("由于在已激活的任务中，没有待重呼数据，故不再扫描待重呼数据到排队机。");
+				StringUtil.log(this, sb.toString());
+				return;
+			}
 			taskIds = taskIds.substring(0, taskIds.length()-1);  //去掉最后的逗号
 			sb.append("当前处理激活状态的任务有:" + activeCallTaskList.size() + ",这些任务的taskId列表是:" + taskIds + ",系统将从这些任务获取待重呼数据。");
 			
