72603a07e6dbe82eef606c49f824b6097f8917d7
hwzyyx <120077407@qq.com>
2019-01-02 11:07:24 +0800

    在 SendActionCallbackForAutoCall.java 中，去掉原来写入日志文件的外呼记录。

--- src/com/callke8/predialqueueforautocallbyquartz/SendActionCallbackForAutoCall.java
+++ src/com/callke8/predialqueueforautocallbyquartz/SendActionCallbackForAutoCall.java
@@ -31,7 +31,7 @@ public class SendActionCallbackForAutoCall implements SendActionCallback {
 		
 		System.out.println("response: " + response);
 		String responseResult = response.getResponse();      //执行外呼Action的结果
-		StringUtil.writeString("/data/autocall_exec_log/autocall_callout.log", DateFormatUtils.getCurrentDate() + "\t 系统发送呼叫Action,通道(Channel)：" + channel + ",Response结果:" + responseResult + ",\tResponse详情:" + response + "\r\n", true);
+		//StringUtil.writeString("/data/autocall_exec_log/autocall_callout.log", DateFormatUtils.getCurrentDate() + "\t 系统发送呼叫Action,通道(Channel)：" + channel + ",Response结果:" + responseResult + ",\tResponse详情:" + response + "\r\n", true);
 		StringUtil.log(this, "系统发送呼叫Action,通道(Channel)：" + channel + ",Response结果:" + responseResult + ",Response详情:" + response);
 		if(responseResult.equalsIgnoreCase("Error")) {       
 			//如果执行的结果为 error，则可以提前将结果更改，尽快的释放外呼资源,这样子,外呼失败的记录，
@@ -40,13 +40,6 @@ public class SendActionCallbackForAutoCall implements SendActionCallback {
 			StringUtil.log(this, "执行外呼 Response结果为:" + responseResult + ",外呼信息:" + actt + " 外呼失败!");
 			
 			//返回所有的此 Response 的内容
-			/*System.out.println("外呼失败，失败时，返回的响应的键值对信息如下：");
-			Map<String,Object> allAttrs = response.getAttributes();
-			Iterator<Map.Entry<String,Object>> iterator = allAttrs.entrySet().iterator();
-			while(iterator.hasNext()) {
-				Map.Entry<String, Object> entry = iterator.next();
-				System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
-			}*/
 			
 			//如果外呼失败，则应该马上释放外呼资源
 			if(AutoCallPredial.activeChannelCount > 0){
